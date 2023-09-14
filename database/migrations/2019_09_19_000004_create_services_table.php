1b1835e09d7f62ccb4f25ba0ef135ec3d73d605d
Anderson <billbardales@gmail.com>
2023-09-14 15:34:53 -0500

    base_de_datos y seeder

--- database/migrations/2019_09_19_000004_create_services_table.php
+++ database/migrations/2019_09_19_000004_create_services_table.php
@@ -21,3 +21,4 @@ class CreateServicesTable extends Migration
         });
     }
 }
+
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,15 @@ class UsersTableSeeder extends Seeder
                 'created_at'     => '2019-09-19 12:08:28',
                 'updated_at'     => '2019-09-19 12:08:28',
             ],
+            [
+                'id'             => 2,
+                'name'           => 'Fressia',
+                'email'          => 'fressia@gmail.com',
+                'password'       => '$2y$10$/r9I9ANLKTgKL1n2X1LR7u.Pf4gxPqgztmh6pLEHh3QTVfJcUt20e',
+                'remember_token' => null,
+                'created_at'     => '2023-09-10 17:46:17',
+                'updated_at'     => '2023-09-10 17:46:17',
+            ],
         ];
 
         User::insert($users);
